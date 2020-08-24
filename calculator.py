d3ce3fae2beebd3ae8325f3f13865d017366e1c1
Nicolai Haug <nicolai.haug@fys.uio.no>
2020-08-25 00:24:11 +0200

    Simple calculator

---
+++ calculator.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+
+def add(x, y):
+    return x + y
---
+++ test_calculator.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+import calculator
+
+
+def test_add():
+    assert calculator.add(2, 1) == 3
